4b4ba3a18c09aaee09063536bbdb11fbd5e49001
MuratOzte <mo2553481@gmail.com>
2024-01-31 19:58:54 +0300

    Refactor home page layout and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,38 @@
-import Image from 'next/image';
-import { Grid } from '@mui/material';
-import { Computer } from '@/assets/homepage-slider-images';
+import { Typography, Grid } from '@mui/material';
 import ImageSlider from '@/components/home/image-slider';
-
 export default function Home() {
     return (
         <>
-            <div
+            <Grid
+                container
                 style={{
                     display: 'flex',
                     justifyContent: 'flex-end',
                     alignItems: 'flex-end',
                 }}
             >
-                <ImageSlider />
-            </div>
+                <Grid item xs={6}>
+                    <ImageSlider />
+                </Grid>
+                <Grid item xs={6} sx={{ position: 'absolute', right: '10%' }}>
+                    <Typography
+                        variant="h2"
+                        component="h2"
+                        gutterBottom
+                        sx={{
+                            fontFamily: 'Work Sans sans-serif',
+                        }}
+                    >
+                        Shop smart,
+                    </Typography>
+                    <Typography variant="h2" component="h2" gutterBottom>
+                        Live stylish,
+                    </Typography>
+                    <Typography variant="h6" component="h6" gutterBottom>
+                        Elevate Your Lifestyle with Our Unbeatable Deals,
+                    </Typography>
+                </Grid>
+            </Grid>
         </>
     );
 }
