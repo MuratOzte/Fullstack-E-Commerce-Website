afdbf9343ce6edff5644afa654397cf324406942
MuratOzte <mo2553481@gmail.com>
2024-02-05 04:14:06 +0300

    Refactor product API routes

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,9 +1,9 @@
 import { MongoClient } from 'mongodb';
 import { NextResponse } from 'next/server';
 
-export async function GET(req: any, res: any) {
+export async function GET() {
     try {
-        const client = await MongoClient.connect(process.env.MONGODB_URI || '');
+        const client = await MongoClient.connect(process.env.MONGODB_URI!);
 
         const db = client.db('e-commerce');
         const productsCollection = db.collection('products');
@@ -16,3 +16,38 @@ export async function GET(req: any, res: any) {
     }
 }
 
+export async function PUT(req: any, res: any) {
+    let client;
+    try {
+        client = await MongoClient.connect(process.env.MONGODB_URI!);
+
+        const data = await req.json();
+
+        const db = client.db('e-commerce');
+        const prodcutsCollection = db.collection('products');
+        await prodcutsCollection.updateMany(
+            { name: data.name },
+            {
+                $set: {
+                    sellCount: data.sellCount,
+                    star: data.star,
+                    ram: data.ram,
+                    storage: data.storage,
+                    battery: data.battery,
+                    comments: [],
+                },
+            }
+        );
+
+        return NextResponse.json({
+            message: 'successfully inserted',
+            data: data,
+        });
+    } catch (e) {
+        console.log(e);
+    } finally {
+        if (client) {
+            await client.close();
+        }
+    }
+}
